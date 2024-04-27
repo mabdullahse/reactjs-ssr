d9b9277e03d0b591c192be4cfee99ea8955a4dcd
Muhammad_abdullah <mabdullah.se@gmail.com>
2024-04-27 14:19:11 +0500

    first commit

---
+++ src/components/HomeModule.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const styles = {
+  container: {
+    maxWidth: "120rem",
+    padding: "0 3.2rem",
+    margin: "0 auto",
+    width: "800px",
+  },
+};
+const HomeModule = () => {
+  return (
+    <div style={styles.container}>
+      <h1>
+        This is my home project as an example for showing to Dejan Jaksic{" "}
+      </h1>
+    </div>
+  );
+};
+
+export default HomeModule;
